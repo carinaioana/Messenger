3b586061cdac3bd48c46cab80e12bed114bef783
carinaioana <carinasrb@icloud.com>
2023-12-30 16:21:54 +0200

    Added symetric encryption(Fernet)

--- client/client.py
+++ client/client.py
@@ -1,26 +1,32 @@
-import datetime
-import os
+import base64
+import io
 import socket
 import threading
 import tkinter as tk
 from tkinter import messagebox
-from PIL import ImageTk, Image
+from cryptography.fernet import Fernet
+import rsa
 
 
-def save_message(client_id, message):
-    folder_path = 'chat_history'
-    os.makedirs(folder_path, exist_ok=True)
-
-    with open(os.path.join(folder_path, "chat_history.txt"), "a") as history_file:
-        history_file.write(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S:')} Client {client_id}: {message}\n")
+DARK_GRAY = "#3a3a3a"
+LIGHT_GRAY = "#f5f5f5"
+WHITE = "#ffffff"
+FONT_NAME = "Helvetica"
+FONT_SIZE = 12
+BUFFER_SIZE = 1024
 
 
 class ChatApplication:
     def __init__(self):
-
         self.client_id = None
-        self.text = None
         self.entry = None
+        self.text = None
+
+        self.public_key, self.private_key = rsa.newkeys(BUFFER_SIZE)
+        self.public_partner = None
+
+        self.fernet_key = None
+        self.fernet_obj = None
 
         self.host = '127.0.0.1'
         self.port = 55555
@@ -34,67 +40,133 @@ class ChatApplication:
         self.create_login_widgets()
 
     def create_login_widgets(self):
-        label = tk.Label(self.root, text="Enter your ID: ")
+        self.root.configure(bg=DARK_GRAY)
+
+        label = tk.Label(self.root, text="Enter your ID: ", bg=DARK_GRAY, fg=WHITE, font=(FONT_NAME, FONT_SIZE))
         label.pack()
 
-        self.entry = tk.Entry(self.root)
+        self.entry = tk.Entry(self.root, bg=LIGHT_GRAY, fg=DARK_GRAY, font=(FONT_NAME, FONT_SIZE))
         self.entry.pack()
 
-        button_login = tk.Button(self.root, text="Login", command=self.login)
-        button_login.pack()
+        self.entry.bind("<Return>", self.login)
 
-    def login(self):
+        button_login = tk.Button(self.root, text="Login", command=self.login, bg=LIGHT_GRAY, fg=DARK_GRAY,
+                                 font=(FONT_NAME, FONT_SIZE))
+        button_login.pack()
+        self.root.protocol("WM_DELETE_WINDOW", self.on_close)
+
+    def login(self, event=None):
+        self.fernet_key = Fernet.generate_key()
+        self.fernet_obj = Fernet(self.fernet_key)
+
         self.client_id = self.entry.get().strip()
         if self.client_id:
+
             self.client.send(self.client_id.encode('utf-8'))
-            self.create_chat_widgets()
+            self.client.send(self.fernet_key)
+
+            server_response = self.client.recv(1024).decode('utf-8')
+
+            if server_response == "This ID is already in use. Please try another ID.":
+                messagebox.showerror("Login Failed", "This ID is already in use. Please try another ID.")
+            else:
+                self.create_chat_widgets()
+                if self.text:
+                    self.text.insert(tk.END, server_response + '\n')
 
     def create_chat_widgets(self):
         self.destroy_widgets()
 
         self.root.title("Chat Application")
+        self.root.configure(bg=DARK_GRAY)
 
-        self.text = tk.Text(self.root)
-        self.text.pack(pady=10)
+        self.text = tk.Text(self.root, bg=LIGHT_GRAY, fg=DARK_GRAY, font=(FONT_NAME, FONT_SIZE))
+        scrollbar = tk.Scrollbar(self.root, command=self.text.yview)
+        self.text.configure(yscrollcommand=scrollbar.set)
+        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        self.text.pack(pady=10, expand=True, fill=tk.BOTH)
 
-        self.entry = tk.Entry(self.root, width=50)
+        self.entry = tk.Entry(self.root, width=50, bg=LIGHT_GRAY, fg=DARK_GRAY, font=(FONT_NAME, FONT_SIZE))
         self.entry.pack(pady=10)
 
-        send_button = tk.Button(self.root, text="Send", command=lambda: self.send(self.entry))
+        # upload_images_button = tk.Button(self.root, text="Upload Images", command=self.upload_images, bg=LIGHT_GRAY,
+        #                                  fg=DARK_GRAY, font=(FONT_NAME, FONT_SIZE))
+        # upload_images_button.pack()
+
+        send_button = tk.Button(self.root, text="Send", command=lambda: self.send(self.entry), bg=LIGHT_GRAY,
+                                fg=DARK_GRAY, font=(FONT_NAME, FONT_SIZE))
         send_button.pack()
 
-        threading.Thread(target=self.receive, args=(self.text,)).start()
+        self.entry.bind("<Return>", self.send)
 
-        self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
+        threading.Thread(target=self.receive_message, daemon=True).start()
+        self.root.protocol("WM_DELETE_WINDOW", self.on_close)
 
-    def receive(self, text_widget):
+    # def upload_images(self):
+    #     f_types = [('JPG files', '*.jpg'), ('PNG files', '*.png')]
+    #     filename = tk.filedialog.askopenfilename(filetypes=f_types)
+    #     img = Image.open(filename)
+    #     img = img.resize((100, 100))
+    #     img = ImageTk.PhotoImage(img)
+    #     e1 = tk.Label(self.root)
+    #     e1.pack()
+    #     e1.image = img
+    #     e1['image'] = img
+    #     self.image_path = filename
+    #
+    # def send_images(self):
+    #     with open(self.image_path, "rb") as image_file:
+    #         image_data = base64.b64encode(image_file.read())
+    #         self.client.send(f"/image {image_data}".encode('utf-8'))
+
+    def send(self, event=None):
+        self.send_message_to_server(self.entry)
+
+    def send_message_to_server(self, entry_widget):
+        message = self.entry.get()
+        recipient_id = ""
+
+        if message:
+            if message.startswith("/private"):
+                self.send_private_message_to_server(message)
+            else:
+                encrypted_message = self.fernet_obj.encrypt(message.encode('utf-8'))
+                self.client.send(encrypted_message)
+
+            if self.text:
+                if message.startswith("/private"):
+                    self.print_sent_private_message(message, recipient_id)
+                else:
+                    self.text.insert(tk.END, f"You: {message}\n")
+
+            entry_widget.delete(0, tk.END)
+
+    def receive_message(self):
         while True:
             try:
-                message = self.client.recv(1024).decode('utf-8')
-                if not message:
+                encrypted_message = self.client.recv(BUFFER_SIZE)
+                if not encrypted_message:
                     break
-                text_widget.insert(tk.END, message + '\n')
-                save_message(self.client_id, message)
+                message = self.fernet_obj.decrypt(encrypted_message).decode('utf-8')
+                if self.text:
+                    print(message)
+                    self.text.insert(tk.END, message + '\n')
             except Exception as e:
                 print(f"Error receiving message from server: {e}")
                 break
 
-    def send(self, entry_widget):
-        message = entry_widget.get()
+    def send_private_message_to_server(self, message):
+        parts = message.split(" ", 2)
+        recipient_id = parts[1] if len(parts) > 1 else None
+        if recipient_id:
+            encrypted_message = self.fernet_obj.encrypt(message.encode('utf-8'))
+            self.client.send(encrypted_message)
 
-        if message:
-            self.text.insert(tk.END, "Client " + str(self.client_id) + ":" + message + '\n')
+    def print_sent_private_message(self, message, recipient_id):
+        message = message[10 + len(recipient_id):]
+        self.text.insert(tk.END, f"Message to {recipient_id}: {message} \n")
 
-            if message.startswith("/private"):
-                parts = message.split(" ", 2)
-                recipient_id = parts[1] if len(parts) > 1 else None
-                if recipient_id:
-                    self.client.send(f"/private {recipient_id} {parts[2]}".encode('utf-8'))
-            else:
-                self.client.send(message.encode('utf-8'))
-            entry_widget.delete(0, tk.END)
-
-    def on_closing(self):
+    def on_close(self):
         try:
             if messagebox.askokcancel("Quit", "Do you want to quit?"):
                 self.client.send("quit".encode('utf-8'))
--- server/server.py
+++ server/server.py
@@ -1,97 +1,150 @@
-import datetime
+import base64
 import os
 import socket
 import threading
+import json
+import logging
+from cryptography.fernet import Fernet
+import rsa
 
-# Configurare server
 host = '127.0.0.1'
 port = 55555
 
-# Creare socket
+BUFFER_SIZE = 1024
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
 server.listen()
 
-# Listă pentru stocarea clienților
 clients = []
 client_ids = {}
 
 
+# Logging the message data from everyone
+def setup_server_logging():
+    folder_path = 'server_chat_history'
+    os.makedirs(folder_path, exist_ok=True)
+    log_file_path = os.path.join(folder_path, f"server_chat_history.json")
+
+    logger = logging.getLogger('server_logger')
+    logger.setLevel(logging.INFO)
+
+    file_handler = logging.FileHandler(log_file_path)
+
+    class JsonFormatter(logging.Formatter):
+        def format(self, record):
+            log_record = {
+                "time": self.formatTime(record, self.datefmt),
+                "message": record.getMessage()
+            }
+            return json.dumps(log_record)
+
+    formatter = JsonFormatter()
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    return logger
+
+
+server_logger = setup_server_logging()
+
+
 def handle_client(client):
+    public_key, private_key = rsa.newkeys(BUFFER_SIZE)
+    public_partner = None
+
+    client_id = client.recv(1024).decode('utf-8')
+
+    if client_id in client_ids.values():
+        client.send("This ID is already in use. Please try another ID.".encode('utf-8'))
+        handle_client(client)
+    else:
+        client_fernet_key = client.recv(1024)
+
+        clients.append((client, client_fernet_key))
+        client_ids[client] = client_id
+        client.send(f"You are connected to the server. You can start chatting, {client_id}.".encode('utf-8'))
+
     while True:
         try:
-            message = client.recv(1024).decode('utf-8')
-
-            if not message:
+            encrypted_message = client.recv(BUFFER_SIZE)
+            if not encrypted_message:
                 break
+
+            fernet_obj = Fernet(client_fernet_key)
+            message = fernet_obj.decrypt(encrypted_message).decode('utf-8')
 
             if message:
                 sender_id = client_ids[client]
-                timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-                folder_path = 'chat_history'
-                os.makedirs(folder_path, exist_ok=True)
-
-                with open(os.path.join(folder_path, f"chat_history_{sender_id}.txt"), "a") as history_file:
-                    history_file.write(f"{timestamp} - {message}\n")
-
-                print(f"Received message from client {sender_id}: {message}")
-
-                # Verifica daca mesajul e un mesaj privat
-                if message.startswith("/private"):
-                    parts = message.split(" ", 2)
-                    if len(parts) == 3:
-                        recipient_id = parts[1]
-                        private_message = parts[2]
-
-                        # Gaseste clientul destinatar și trimite mesajul privat
-                        for c in clients:
-                            if c != client and client_ids[c] == recipient_id:
-                                try:
-                                    c.send(f"Private message from {sender_id}: {private_message}".encode('utf-8'))
-                                except Exception as e:
-                                    print(f"Error sending private message to client {recipient_id}: {e}")
-                                    remove_client(c)
+                if message == "quit":
+                    remove_client(client)
+                    break
+                elif message.startswith("/private"):
+                    send_private_message(message, client, sender_id)
+                    break
                 else:
-                    # Trimite mesajul la toti clientii
-                    for c in clients:
-                        if c != client:
-                            try:
-                                c.send(f"Client {sender_id}: {message}".encode('utf-8'))
-                            except Exception as e:
-                                print(f"Error broadcasting message to client {client_ids[c]}: {e}")
-                                remove_client(c)
+                    send_message(message, client, sender_id)
         except Exception as e:
-            print(f"Error handling client {client_ids[client]}: {e}")
             remove_client(client)
+            print(f"Error handling client {client_ids.get(client, 'unknown')}: {e}")
             break
 
 
-# Funcție pentru eliminarea unui client
+def send_message(message, sender, sender_id):
+    for c, key in clients:
+        print(key)
+        if c != sender:
+            try:
+                fernet_obj = Fernet(key)
+                encrypted_message = fernet_obj.encrypt(f"Client {sender_id}: {message}".encode('utf-8'))
+                c.send(encrypted_message)
+                server_logger.info(f"Message from {sender_id}: {message}")
+            except Exception as e:
+                print(f"Error broadcasting message to client {client_ids[c]}: {e}")
+
+
+def send_private_message(message, sender, sender_id):
+    parts = message.split(" ", 2)
+    if len(parts) == 3:
+        recipient_ids = parts[1].split(",")
+        private_message = parts[2]
+
+        for recipient_id in recipient_ids:
+            for c,key in clients:
+                if c != sender and client_ids[c] == recipient_id.strip():
+                    try:
+                        fernet_obj = Fernet(key)
+                        encrypted_private_message = fernet_obj.encrypt(f"Private message from {sender_id}: {private_message}".encode('utf-8'))
+                        c.send(encrypted_private_message)
+                    except Exception as e:
+                        print(f"Error sending private message to client {recipient_id}: {e}")
+                        remove_client(c)
+    else:
+        sender.send(
+            "Invalid /private command usage. Please provide recipient IDs and a message.".encode(
+                'utf-8'))
+
+
 def remove_client(client):
-    if client in clients:
-        client_id = client_ids[client]
-        print(f"Client {client_id} disconnected")
-        clients.remove(client)
+    print(f"Client {client_ids[client]} disconnected")
+    if client in client_ids:
         del client_ids[client]
-        client.close()
+
+    for c, key in clients:
+        if c == client:
+            clients.remove((c, key))
+            break
+
+    client.close()
 
 
 def main():
-    # Așteaptă conexiuni și creează un fir de execuție pentru fiecare client
     while True:
         client, address = server.accept()
         print(f"Connection from {address} established")
 
-        client_id = client.recv(1024).decode('utf-8')
-
-        # Adaugă clientul la listă și atribuie un id
-        clients.append(client)
-        client_ids[client] = client_id
-
-        client.send("Connected to the server. You can start chatting.".encode('utf-8'))
-
-        threading.Thread(target=handle_client, args=(client,)).start()
+        threading.Thread(target=handle_client, args=(client,), daemon=True).start()
 
 
 if __name__ == "__main__":
