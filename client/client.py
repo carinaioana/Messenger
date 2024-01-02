077fbec87de2b646c7086665ca3068f7aaefade1
carinaioana <carinasrb@icloud.com>
2024-01-02 23:13:37 +0200

    Added emoji picker

--- client/client.py
+++ client/client.py
@@ -1,11 +1,14 @@
 import base64
 import io
+import json
 import os.path
 import socket
 import threading
 import time
 import tkinter as tk
-from tkinter import messagebox, filedialog
+from tkinter import *
+from tkinter import ttk, filedialog, messagebox
+import emoji
 
 from PIL import Image, ImageTk
 from cryptography.fernet import Fernet
@@ -77,13 +80,13 @@ class ChatApplication:
             if server_response == "This ID is already in use. Please try another ID.":
                 messagebox.showerror("Login Failed", "This ID is already in use. Please try another ID.")
             else:
-                self.create_chat_widgets()
+                self.create_chat_window()
                 if self.text:
                     self.text.config(state=tk.NORMAL)
                     self.text.insert(tk.END, server_response + '\n')
                     self.text.config(state=tk.DISABLED)
 
-    def create_chat_widgets(self):
+    def create_chat_window(self):
         self.destroy_widgets()
 
         self.root.title("Chat Application")
@@ -108,13 +111,54 @@ class ChatApplication:
                                       fg=DARK_BLUE, font=(FONT_NAME, FONT_SIZE))
         send_image_button.pack()
 
+        emoji_button = tk.Button(self.root, text="Emoji List", command=self.emoji_window, bg=WHITE,
+                                 fg=DARK_BLUE, font=(FONT_NAME, FONT_SIZE))
+        emoji_button.pack()
         self.entry.bind("<Return>", self.send)
 
         threading.Thread(target=self.receive_message, daemon=True).start()
         self.root.protocol("WM_DELETE_WINDOW", self.on_close)
 
-    def send_image(self, ):
-        filepath = filedialog.askopenfilename()
+    def emoji_window(self):
+        emoji_data = {}
+
+        folder_path = os.path.abspath("files")
+        full_path = folder_path + "\\emoji_list"
+
+        with open(full_path, 'r', encoding='utf-8') as file:
+            for line in file:
+                parts = line.strip().split('\t')
+                if len(parts) == 2:
+                    emoji, description = parts
+                    emoji_data[emoji] = description
+
+        emoji_window = Toplevel(self.root)
+        emoji_window.geometry("750x250")
+        emoji_window.title("Emoji List")
+        emoji_label = tk.Label(emoji_window, text="Choose an emoji and click on it to put it in your conversation",
+                               bg=LIGHT_BLUE, fg=DARK_BLUE, font=(FONT_NAME, FONT_SIZE))
+        emoji_label.pack()
+
+        emoji_text = tk.Text(emoji_window, bg=WHITE, fg=DARK_BLUE, font=(FONT_NAME, FONT_SIZE))
+        emoji_text.pack()
+
+        for emoji_char in emoji_data:
+            emoji_text.insert(END, emoji_char + ' ' + emoji_data[emoji_char] + "\n")
+
+        emoji_text.bind("<Button-1>", self.insert_emoji)
+
+        emoji_text.config(state=tk.DISABLED)
+
+    def insert_emoji(self, event):
+        emoji_text = event.widget
+        clicked_index = emoji_text.index(f"@{event.x},{event.y}")
+        line = emoji_text.get(f"{clicked_index} linestart", f"{clicked_index} lineend")
+        emoji_char = line.split()[0]
+
+        self.entry.insert(tk.END, emoji_char)
+
+    def send_image(self):
+        filepath = filedialog.askopenfilename(filetypes=[('Jpg Files', '*.jpg'), ('PNG Files', '*.png')])
         if filepath:
             self.client.send(self.fernet_obj.encrypt(b"/image"))
             with open(filepath, 'rb') as image_file:
